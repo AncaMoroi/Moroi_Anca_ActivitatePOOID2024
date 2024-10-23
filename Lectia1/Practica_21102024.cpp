5c10fd8b27a3a532b9624b4d6c875e39537ee426
Anca <moroianca22@stud.ase.ro>
2024-10-23 20:56:51 +0300

    23.10.2024

--- Lectia1/Practica_21102024.cpp
+++ Lectia1/Practica_21102024.cpp
@@ -3,45 +3,55 @@
 
 using namespace std;
 
-class Masina {
+class Bloc {
 public:
-	string marca;
-	int anFabricatie;
+	int nrApartamente;
+	char* adresa;
+	string denumire;
 	float pret;
-	char* culoare;
 
-	//constructor fara parametri
-
-	Masina() {
-		this->marca = "";
-		this->anFabricatie = 0;
+	//constructor fara parametrii
+	Bloc() {
+		this->nrApartamente = 0;
+		this->adresa = new char[strlen("adresa") + 1];
+		strcpy_s(this->adresa, strlen("culoare") + 1, "culoare");
+		this->denumire = "";
 		this->pret = 0.0f;
-		this->culoare = new char[strlen("culoare") + 1];
-		strcpy_s(this->culoare, strlen("culoare") + 1, "culoare");
+
 
 	}
 
-	//metoda afiare
-	void afisare() {
-		cout << "Marca:" << this->marca << endl;
-		cout << "Anul Fabricatiei: " << this->anFabricatie << endl;
-		cout << "Pret: " << this->pret << endl;
-		cout << "Culoare: " << this->culoare << endl;
+	//constructor cu parametri
+	Bloc(string denumire) {
+		this->denumire = denumire;
+		this->nrApartamente = 0;
+		this->adresa = new char[strlen(adresa) + 1];
+		strcpy_s(this->adresa, strlen(adresa) + 1, adresa);
+		this->pret = 0.0f;
+	}
 
+	//constructor totii parametrii
+
+
+	//afisare
+
+	void afisare() {
+		cout << "Nume: " << this->denumire << endl;
+		cout << "Nr Apartamente: " << this->nrApartamente << endl;
+		cout << "Adresa: " << this->adresa << endl;
+		cout << "Pret: " << this->pret << endl;
 	}
 
 };
 
 void main() {
-	Masina masina1;
-	masina1.marca = "Honda";
-	masina1.anFabricatie = 2016;
-	masina1.pret = 20.5;
-	masina1.culoare = new char[strlen("Neagra") + 1];
-	strcpy_s(masina1.culoare, strlen("Neagra") + 1, "Neagra");
+	Bloc b1;
+	b1.denumire = "Atena";
+	b1.adresa = new char[strlen("Bucuresti nr 19") + 1];
+	strcpy_s(b1.adresa, strlen("Bucuresti nr 19") + 1, "Bucuresti nr 19");
+	b1.nrApartamente = 80;
+	b1.pret = 80.000f;
 
-	masina1.afisare();
-
-	Masina masina2;
+	b1.afisare();
 
 }
